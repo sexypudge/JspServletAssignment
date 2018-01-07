2c669b3ea07d482d3f2b6a975628090144d1ea01
Nguyễn Lương <luong1c11@gmail.com>
2018-01-07 22:21:15 +0700

    1st commit

---
+++ src/main/java/com/nissho/vn/model/Department.java
@@ -0,0 +1,43 @@
+package com.nissho.vn.model;
+
+public class Department {
+	private int deptId;
+	private String deptName;
+	private String dirName;
+	 
+	
+	public Department() {
+		super();
+	}
+	public Department(int deptId, String deptName, String dirName) {
+		super();
+		this.deptId = deptId;
+		this.deptName = deptName;
+		this.dirName = dirName;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getDirName() {
+		return dirName;
+	}
+	public void setDirName(String dirName) {
+		this.dirName = dirName;
+	}
+	@Override
+	public String toString() {
+		return "Department [deptId=" + deptId + ", deptName=" + deptName + ", dirName=" + dirName + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/nissho/vn/servlet/DepartmentServlet.java
@@ -0,0 +1,5 @@
+package com.nissho.vn.servlet;
+
+public class DepartmentServlet {
+
+}
